a328e4e53caf23b03210046f162d223d24697d50
Eduardo Breno <eduardobreno@msn.com>
2019-06-21 20:51:23 -0300

    refactor: change questionItem tu stateless component

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,41 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { Grid, Button, Card, Image } from 'semantic-ui-react';
 
-class QuestionItem extends Component {
 
-    render() {
-        const { id, author, avatarURL, optionOne, optionTwo } = this.props.item;
-
-        return (
-            <Card centered>
-                <Card.Content>
-                    <Image floated='left' size='mini' src={avatarURL} />
-                    <Card.Header>{author}</Card.Header>
-                    <Card.Meta>asks</Card.Meta>
-                    <Card.Description>
-                        <Grid>
-                            <Grid.Row columns={2} divided>
-                                <Grid.Column style={{ textAlign: 'right' }}>
-                                    {optionOne.text}
-                                </Grid.Column>
-                                <Grid.Column style={{ textAlign: 'left' }}>
-                                    {optionTwo.text}
-                                </Grid.Column>
-                            </Grid.Row>
-                        </Grid>
-                    </Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                    <Button fluid color='blue' onClick={() => {
-                        this.props.history.push(`questions/${id}`);
-                    }}>
-                        View Poll
+const QuestionItem = ({ item, history }) => {
+    const { id, author, avatarURL, optionOne, optionTwo } = item;
+    return (
+        <Card centered>
+            <Card.Content>
+                <Image floated='left' size='mini' src={avatarURL} />
+                <Card.Header>{author}</Card.Header>
+                <Card.Meta>asks</Card.Meta>
+                <Card.Description>
+                    <Grid>
+                        <Grid.Row columns={2} divided>
+                            <Grid.Column style={{ textAlign: 'right' }}>
+                                {optionOne.text}
+                            </Grid.Column>
+                            <Grid.Column style={{ textAlign: 'left' }}>
+                                {optionTwo.text}
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
+                </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+                <Button fluid color='blue' onClick={() => {
+                    history.push(`questions/${id}`);
+                }}>
+                    View Poll
                     </Button>
-                </Card.Content>
-            </Card >
-        );
-    }
+            </Card.Content>
+        </Card >
+    );
 }
 
 export default withRouter(QuestionItem);
